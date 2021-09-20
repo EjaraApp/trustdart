8725cccaccccfcded824e2f0107a1837b3cc137d
Baah Kusi <seth@aims.ac.za>
2021-09-20 15:35:26 +0000

    ethereum and tezos transaction signing complete for android

--- android/src/main/kotlin/africa/ejara/trustdart/TrustdartPlugin.kt
+++ android/src/main/kotlin/africa/ejara/trustdart/TrustdartPlugin.kt
@@ -1,6 +1,7 @@
 package africa.ejara.trustdart
 
 import androidx.annotation.NonNull
+import org.json.JSONObject
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -12,6 +13,7 @@ import io.flutter.plugin.common.MethodChannel.Result
 import wallet.core.jni.HDWallet
 import wallet.core.jni.CoinType
 import wallet.core.jni.BitcoinAddress
+import wallet.core.java.AnySigner
 
 /** TrustdartPlugin */
 class TrustdartPlugin: FlutterPlugin, MethodCallHandler {
@@ -56,7 +58,7 @@ class TrustdartPlugin: FlutterPlugin, MethodCallHandler {
           val address: Map<String, String?>? = generateAddressForCoin(path, coin)
           if (address == null) result.error("address_null", "failed to generate address", null) else result.success(address)
         } else {
-          result.error("arguments_null", "$path and $coin cannot be null", null)
+          result.error("arguments_null", "[path] and [coin] cannot be null", null)
         }
       }
       "validateAddressForCoin" -> {
@@ -69,8 +71,16 @@ class TrustdartPlugin: FlutterPlugin, MethodCallHandler {
           result.error("arguments_null", "$address and $coin cannot be null", null)
         }
       }
-      "buildTransaction" -> {
-        result.notImplemented()
+      "buildAndSignTransaction" -> {
+        val coin: String? = call.argument("coin")
+        val path: String? = call.argument("path")
+        val txData: Map<String, String>? = call.argument("txData")
+        if (txData != null && coin != null && path != null) {
+          val txHash: String? = buildAndSignTransaction(coin, path, txData)
+          if (txHash == null) result.error("txhash_null", "failed to buid and sign transaction", null) else result.success(txHash)
+        } else {
+          result.error("arguments_null", "[txData], [coin] and [path] cannot be null", null)
+        }
       }
       else -> result.notImplemented()
     }
@@ -81,17 +91,19 @@ class TrustdartPlugin: FlutterPlugin, MethodCallHandler {
   }
 
   private fun generateAddressForCoin(path: String, coin: String): Map<String, String?>? {
-    val privateKey = wallet.getKey(path)
     return when(coin) {
       "BTC" -> {
+        val privateKey = wallet.getKey(CoinType.BITCOIN, path)
         val publicKey = privateKey.getPublicKeySecp256k1(true)
         val address = BitcoinAddress(publicKey, CoinType.BITCOIN.p2pkhPrefix())
         mapOf("legacy" to address.description(), "segwit" to CoinType.BITCOIN.deriveAddress(privateKey))
       }
       "ETH" -> {
+        val privateKey = wallet.getKey(CoinType.ETHEREUM, path)
         mapOf("legacy" to CoinType.ETHEREUM.deriveAddress(privateKey))
       }
       "XTZ" -> {
+        val privateKey = wallet.getKey(CoinType.TEZOS, path)
         mapOf("legacy" to CoinType.TEZOS.deriveAddress(privateKey))
       }
       else -> null
@@ -111,5 +123,39 @@ class TrustdartPlugin: FlutterPlugin, MethodCallHandler {
       }
       else -> false
     }
+  }
+
+  private fun buildAndSignTransaction(coin: String, path: String, txData: Map<String, String>): String? {
+    return when(coin) {
+      "XTZ" -> {
+        buildAndSignTezosTransaction(path, txData)
+      }
+      "ETH" -> {
+        buildAndSignEthereumTransaction(path, txData)
+      }
+      "BTC" -> {
+        buildAndSignBitcoinTransaction(path, txData)
+      }
+      else -> null
+    }
+  }
+
+  private fun buildAndSignTezosTransaction(path: String, txData: Map<String, String>): String? {
+    val privateKey = wallet.getKey(CoinType.TEZOS, path)
+    val opJson =  JSONObject(txData).toString();
+    val result = AnySigner.signJSON(opJson, privateKey.data(), CoinType.TEZOS.value())
+    return result
+  }
+
+  private fun buildAndSignEthereumTransaction(path: String, txData: Map<String, String>): String? {
+    val privateKey = wallet.getKey(CoinType.ETHEREUM, path)
+    val opJson =  JSONObject(txData).toString();
+    val result = AnySigner.signJSON(opJson, privateKey.data(), CoinType.ETHEREUM.value())
+    return result
+  }
+
+  private fun buildAndSignBitcoinTransaction(path: String, txData: Map<String, String>): String? {
+    val privateKey = wallet.getKey(CoinType.BITCOIN, path)
+    return null;
   }
 }
