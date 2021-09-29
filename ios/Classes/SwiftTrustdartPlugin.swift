67ac8136209e3a04bfbf3ad305d3421265916ec8
Baah Kusi <seth@aims.ac.za>
2021-09-29 13:54:20 +0000

    Generate and Validate Address, IoS'

--- ios/Classes/SwiftTrustdartPlugin.swift
+++ ios/Classes/SwiftTrustdartPlugin.swift
@@ -29,13 +29,139 @@ public class SwiftTrustdartPlugin: NSObject, FlutterPlugin {
                 wallet = HDWallet(mnemonic: mnemonic, passphrase: "")
                 result(true)
             case "generateAddressForCoin":
-                result(FlutterMethodNotImplemented)
+                let args = call.arguments as! [String: String]
+                let path: String? = args["path"]
+                let coin: String? = args["coin"]
+                if path != nil && coin != nil {
+                    let address: [String: String]? = generateAddressForCoin(path: path!, coin: coin!)
+                    if address == nil {
+                        result(FlutterError(code: "address_null",
+                                            message: "Failed to generate address",
+                                            details: nil))
+                    } else {
+                        result(address)
+                    }
+                } else {
+                    result(FlutterError(code: "arguments_null",
+                                        message: "[path] and [coin] cannot be null",
+                                        details: nil))
+                }
             case "validateAddressForCoin":
-                result(FlutterMethodNotImplemented)
+                let args = call.arguments as! [String: String]
+                let address: String? = args["address"]
+                let coin: String? = args["coin"]
+                if address != nil && coin != nil {
+                    let isValid: Bool = validateAddressForCoin(address: address!, coin: coin!)
+                    result(isValid)
+                } else {
+                    result(FlutterError(code: "arguments_null",
+                                        message: "[address] and [coin] cannot be null",
+                                        details: nil))
+                }
             case "buildAndSignTransaction":
-                result(FlutterMethodNotImplemented)
+                let args = call.arguments as! [String: Any]
+                let coin: String? = args["coin"] as? String
+                let path: String? = args["path"] as? String
+                let txData: [String: String]? = args["txData"] as? [String: String]
+                if coin != nil && path != nil && txData != nil {
+                    let txHash: String? = buildAndSignTransaction(coin: coin!, path: path!, txData: txData!)
+                    if txHash == nil {
+                        result(FlutterError(code: "txhash_null",
+                                            message: "Failed to buid and sign transaction",
+                                            details: nil))
+                    } else {
+                        result(txHash)
+                    }
+                } else {
+                    result(FlutterError(code: "arguments_null",
+                                        message: "[coin], [path] and [txData] cannot be null",
+                                        details: nil))
+                }
             default:
                 result(FlutterMethodNotImplemented)
             }
   }
+    
+    func generateAddressForCoin(path: String, coin: String) -> [String: String]? {
+        var addressMap: [String: String]?
+        let privateKey = wallet!.getKey(derivationPath: path)
+        switch coin {
+        case "BTC":
+            let publicKey = privateKey.getPublicKeySecp256k1(compressed: true)
+            let legacyAddress = BitcoinAddress(publicKey: publicKey, prefix: 0)
+            let scriptHasgAddress = BitcoinAddress(publicKey: publicKey, prefix: 5)
+            addressMap = ["legacy": legacyAddress.description,
+                          "segwit": CoinType.bitcoin.deriveAddress(privateKey: privateKey),
+                          "p2sh": scriptHasgAddress.description,
+            ]
+        case "ETH":
+            addressMap = ["legacy": CoinType.ethereum.deriveAddress(privateKey: privateKey)]
+        case "XTZ":
+            addressMap = ["legacy": CoinType.tezos.deriveAddress(privateKey: privateKey)]
+        default:
+            addressMap = nil
+        }
+        return addressMap
+        
+    }
+    
+    func validateAddressForCoin(address: String, coin: String) -> Bool {
+        var isValid: Bool
+        switch coin {
+        case "BTC":
+            isValid = CoinType.bitcoin.validate(address: address)
+        case "ETH":
+            isValid = CoinType.ethereum.validate(address: address)
+        case "XTZ":
+            isValid = CoinType.tezos.validate(address: address)
+        default:
+            isValid = false
+        }
+        return isValid
+
+    }
+    
+    func buildAndSignTransaction(coin: String, path: String, txData: [String: String]) -> String? {
+        var txHash: String?
+        switch coin {
+        case "BTC":
+            txHash = buildAndSignBitcoinTransaction(path: path, txData: txData)
+        case "ETH":
+            txHash = buildAndSignEthereumTransaction(path: path, txData: txData)
+        case "XTZ":
+            txHash = buildAndSignTezosTransaction(path: path, txData: txData)
+        default:
+            txHash = nil
+        }
+        return txHash
+
+    }
+    
+    private func objToJson(from object:Any) -> String? {
+        guard let data = try? JSONSerialization.data(withJSONObject: object, options: []) else {
+            return nil
+        }
+        return String(data: data, encoding: String.Encoding.utf8)
+    }
+    
+     func buildAndSignTezosTransaction(path: String, txData:  [String: String]) -> String? {
+//        let privateKey = wallet!.getKey(derivationPath: path)
+//        let opJson =  objToJson(from:txData)
+//
+//        let result = AnySigner.signJSON(opJson, privateKey.data, CoinType.tezos.rawValue)
+        return "xtz"
+      }
+
+    func buildAndSignEthereumTransaction(path: String, txData:  [String: String]) -> String? {
+//        let privateKey = wallet!.getKey(derivationPath: path)
+//        let opJson =  objToJson(from:txData)
+//        let result = EthereumSigner.s(opJson, privateKey.data, CoinType.ethereum.rawValue)
+//        return result
+        return "eth"
+      }
+
+    func buildAndSignBitcoinTransaction(path: String, txData:  [String: String]) -> String? {
+//        val privateKey = wallet.getKey(CoinType.BITCOIN, path)
+        return "btc"
+      }
 }
