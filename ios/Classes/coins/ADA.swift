5f259b5f495bba7e183d17fe77a0cfc83af7f09c
Rakeally <40327144+Rakeally@users.noreply.github.com>
2023-06-26 18:58:42 +0100

    updated cardano implementation

--- ios/Classes/coins/ADA.swift
+++ ios/Classes/coins/ADA.swift
@@ -14,23 +14,25 @@ class ADA: Coin  {
         let utxos: [[String: Any]] = txData["utxos"] as! [[String: Any]]
         var listOfUtxos: [CardanoTxInput] = []
 
-        for utx in utxos {
-            listOfUtxos.append(CardanoTxInput.with {
-                $0.outPoint.txHash = Data(hexString: (utx["txid"] as! String))!
-                $0.outPoint.outputIndex = utx["index"] as! UInt64
-                $0.address = utx["senderAddress"] as! String
-                $0.amount = utx["amount"] as! UInt64
-            })
-        }
-
+        
         var input = CardanoSigningInput.with {
             $0.transferMessage.toAddress = txData["receiverAddress"] as! String
             $0.transferMessage.changeAddress =  txData["senderAddress"] as! String
             $0.transferMessage.amount = txData["amount"] as! UInt64
-            $0.ttl = 53333333
-            $0.privateKey = [privateKey!.data]
-            $0.utxos = listOfUtxos
+            $0.ttl = txData["ttl"] as! UInt64
         }
+        input.privateKey.append(privateKey!.data)
+
+        for utx in utxos {
+            var utxo = CardanoTxInput.with {
+                $0.outPoint.txHash = Data(hexString: (utx["txid"] as! String))!
+                $0.outPoint.outputIndex = utx["index"] as! UInt64
+                $0.address = utx["senderAddress"] as! String
+                $0.amount = utx["balance"] as! UInt64
+            }
+            input.utxos.append(utxo)
+        }
+
 
         let output: CardanoSigningOutput = AnySigner.sign(input: input, coin: .cardano)
         return output.encoded.hexString
