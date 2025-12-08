48c6c771b22f48932226cfb7d4cb0ddcbcad9aa3
Jay <jay@sperixlabs.org>
2025-12-08 13:12:02 +0000

    updates to walletcore

--- ios/trustdart/Package.swift
+++ ios/trustdart/Package.swift
@@ -12,23 +12,14 @@ let package = Package(
         .library(name: "trustdart", targets: ["trustdart"])
     ],
     dependencies: [
-        .package(name: "WalletCore", path: "wallet-core"),
-        .package(name: "Starknet", path: "starknet.swift")
+        .package(url: "https://github.com/trustwallet/wallet-core", from: "4.4.4")
     ],
     targets: [
         .target(
             name: "trustdart",
-            dependencies: [],
-            resources: [
-                // If your plugin requires a privacy manifest, for example if it uses any required
-                // reason APIs, update the PrivacyInfo.xcprivacy file to describe your plugin's
-                // privacy impact, and then uncomment these lines. For more information, see
-                // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
-                // .process("PrivacyInfo.xcprivacy"),
-
-                // If you have other resources that need to be bundled with your plugin, refer to
-                // the following instructions to add them:
-                // https://developer.apple.com/documentation/xcode/bundling-resources-with-a-swift-package
+            dependencies: [
+                .product(name: "WalletCore", package: "wallet-core"),
+                .product(name: "WalletCoreSwiftProtobuf", package: "wallet-core")
             ]
         )
     ]
